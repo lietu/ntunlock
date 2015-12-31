92e8918a26f6490ac4a0e191497ff0f9a63ea6ad
Janne Enberg <janne.enberg@lietu.net>
2015-12-31 11:59:57 +0200

    Fixed Steam path on OSX

--- ntunlock/settings_darwin.go
+++ ntunlock/settings_darwin.go
@@ -8,7 +8,7 @@ import (
 func GetPlatformSettings() *Settings {
 	s := Settings{[]string{}}
 	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/Library/Application Support/com.vlambeer.nuclearthrone", os.Getenv("HOME")))
-	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/Steam/%s", os.Getenv("HOME"), SteamAppPath))
+	s.SearchPaths = append(s.SearchPaths, fmt.Sprintf("%s/Library/Application Support/Steam/%s", os.Getenv("HOME"), SteamAppPath))
 
 	return &s
 }
